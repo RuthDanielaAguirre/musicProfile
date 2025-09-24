893fae856a53945eaa2414569f719916deb8f3e3
RuthDanielaAguirre <148866258+RuthDanielaAguirre@users.noreply.github.com>
2025-09-24 11:15:14 +0200

    adding tabs

---
+++ includes/tabs.php
@@ -0,0 +1,10 @@
+<nav class="tabs">
+    <ul>
+        <li><a href="sections/profile.php">About Me</a></li>
+        <li><a href="sections/music.php">My Music</a></li>
+        <li><a href="sections/movies.php">My Movies</a></li>
+        <li><a href="sections/menu.php">Foodie Love</a></li>
+        <li><a href="sections/event.php">My Events</a></li>
+        <li><a href="sections/travel.php">My NextTravel</a></li>
+    </ul>
+</nav>
