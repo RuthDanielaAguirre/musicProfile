6ed3e2c90746c696d636d61419cadfb9abf36ee2
RuthDanielaAguirre <148866258+RuthDanielaAguirre@users.noreply.github.com>
2025-09-25 21:35:48 +0200

    fix: update page title and tab links to use fragment identifiers

--- includes/tabs.php
+++ includes/tabs.php
@@ -1,10 +1,10 @@
 <nav class="tabs">
     <ul>
-        <li><a href="#music.php">My Music</a></li>
-        <li><a href="#profile.php">About Me</a></li>
-        <li><a href="#movies.php">My Movies</a></li>
-        <li><a href="#menu.php">Foodie Love</a></li>
-        <li><a href="#event.php">My Events</a></li>
-        <li><a href="#travel.php">My NextTravel</a></li>
+        <li><a href="#profile" class="panel active">About Me</a></li>
+        <li><a href="#music" class="panel">My Music</a></li>
+        <li><a href="#movies" class="panel">My Movies</a></li>
+        <li><a href="#menu" class="panel">Foodie Love</a></li>
+        <li><a href="#event" class="panel">My Events</a></li>
+        <li><a href="#travel" class="panel">My NextTravel</a></li>
     </ul>
 </nav>
