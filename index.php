91819039ef39f9124ddc50bb8c85eb32843554ce
RuthDanielaAguirre <148866258+RuthDanielaAguirre@users.noreply.github.com>
2025-09-25 10:11:03 +0200

    adding routes to index

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-echo "<h1>Hello, World!</h1>";
+include 'includes/header.php';
+?>
+<div id="paneles">
+    <div id="profile">
+        <?php include 'includes/profile.php'; ?>
+    </div>
+    <div id="music">
+        <?php include 'includes/music.php'; ?>
+    </div>
+    <div id="movies">
+        <?php include 'includes/movies.php'; ?>
+    </div>
+    <div id="menu">
+        <?php include 'includes/menu.php'; ?>
+    </div>
+    <div id="event">
+        <?php include 'includes/event.php'; ?>
+    </div>
+    <div id="travel">
+        <?php include 'includes/travel.php'; ?>
+    </div>
+</div>
+
+<?php
+include 'includes/footer.php';
 ?>
