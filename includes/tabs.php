058d7e32a3d042a68babef712abfa33ce52dab2f
RuthDanielaAguirre <148866258+RuthDanielaAguirre@users.noreply.github.com>
2025-09-25 21:46:44 +0200

    fix: remove unnecessary class from tab links in tabs.php

--- includes/tabs.php
+++ includes/tabs.php
@@ -1,10 +1,10 @@
 <nav class="tabs">
     <ul>
-        <li><a href="#profile" class="panel active">About Me</a></li>
-        <li><a href="#music" class="panel">My Music</a></li>
-        <li><a href="#movies" class="panel">My Movies</a></li>
-        <li><a href="#menu" class="panel">Foodie Love</a></li>
-        <li><a href="#event" class="panel">My Events</a></li>
-        <li><a href="#travel" class="panel">My NextTravel</a></li>
+        <li><a href="#profile" >About Me</a></li>
+        <li><a href="#music" >My Music</a></li>
+        <li><a href="#movies" >My Movies</a></li>
+        <li><a href="#menu" >Foodie Love</a></li>
+        <li><a href="#event" >My Events</a></li>
+        <li><a href="#travel" >My NextTravel</a></li>
     </ul>
 </nav>
