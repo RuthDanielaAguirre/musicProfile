c7469db0c8dce8effd82fc19b44deb27df9a44a9
Ruth Daniela Aguirre López <ruthagulop@campus.monlau.com>
2025-09-30 19:28:44 +0200

    refactor: mover archivos del proyecto a la carpeta public/

---
+++ public/includes/tabs.php
@@ -0,0 +1,10 @@
+<nav class="tabs">
+    <ul>
+        <li><a href="#" data-tab="profile">About Me</a></li>
+        <li><a href="#" data-tab="music">My Music</a></li>
+        <li><a href="#" data-tab="movies">My Movies</a></li>
+        <li><a href="#" data-tab="menu">Foodie Love</a></li>
+        <li><a href="#" data-tab="event">My Events</a></li>
+        <li><a href="#" data-tab="travel">My NextTravel</a></li>
+    </ul>
+</nav>
