92ee7a0ffee606bc9e39cde6f45b0a06df208b63
RuthDanielaAguirre <148866258+RuthDanielaAguirre@users.noreply.github.com>
2025-10-10 11:36:11 +0200

    fixing styles

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,9 +1,11 @@
 </main>
   <footer>
-    <div>
-        <img src="assets/img/logo.png" alt="Logo">
-    </div>
+   <div class="container">
+      <div class="logoContainer">
+        <img src="assets/img/logo.png" alt="logo" class="logo">
+      </div>
     <p>&copy; 2025 My Profile</p>
+  </div>
   </footer>
 </body>
 </html>
